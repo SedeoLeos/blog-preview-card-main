b489da90fa0b0f886ec483aaa4b6ca021fe0f5e8
King sedeo leos <smatsoula19@gmail.com>
2023-12-24 16:27:41 +0100

    add main.js

---
+++ faq-accordion-main/main.js
@@ -0,0 +1,13 @@
+function showMore(accordion){
+    // const clickQestion = questionBox.querySelector(".question");
+    const showAnswer = accordion.querySelector("#answer");
+    const changeIcon = accordion.querySelector("#image-icon");
+
+    if(showAnswer.classList.contains("hidden")){
+        showAnswer.classList.remove("hidden");
+        changeIcon.src="assets/images/icon-plus.svg";
+    }else{
+        showAnswer.classList.add("hidden");
+        changeIcon.src="assets/images/icon-minus.svg";
+    }
+}
